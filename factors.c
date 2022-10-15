df52ab289f0526d1f23f2d5da0bfbb430dfc9458
Peter Chibunna <peterchibunna@gmail.com>
2022-10-15 18:01:20 +0100

    modi..f

--- factors.c
+++ factors.c
@@ -3,28 +3,29 @@
 #include <stdlib.h>
 
 /*unsigned long long*/
-void *factorize(unsigned long long n)  
+void *factorize(unsigned long long n)
 {
 	unsigned long long i;
-	/*static unsigned long long r[2];*/
+	static unsigned long long r[2];
 
 	for (i = 2; i < n; i++)
 	{
 		if (n % i == 0)
 		{
-			/*r[1] = (unsigned long long)i;*/
-			/*r[0] = (unsigned long long)n / i;*/
-			printf("%llu=%llu*%llu\n", n, (unsigned long long)n / i, (unsigned long long)i);
-			return (0);
-			/*return (r);*/
+			r[1] = (unsigned long long)i;
+			r[0] = (unsigned long long)n / i;
+			/*printf("%llu=%llu*%llu\n", n, 
+			(unsigned long long)n / i, (unsigned long long)i);
+			return (0);*/
+			return (r);
 		}
 	}
 
-	/*r[1] = (unsigned long long)1;*/
-	/*r[0] = (unsigned long long)n;*/
-	printf("%llu=%llu*%llu\n", n, (unsigned long long)n, (unsigned long long)1);
-	return (0);
-	/*return (r);*/
+	r[1] = (unsigned long long)1;
+	r[0] = (unsigned long long)n;
+	/*printf("%llu=%llu*%d\n", n, (unsigned long long)n, 1);
+	return (0);*/
+	return (r);
 }
 
 
@@ -53,9 +54,9 @@ int main(int argc, char *argv[])
 	{
 		number = strtoull(line, &ptr, 10);
 
-		factorize(number);
-		/*factors = factorize(number);*/
-		/*printf("%llu=%llu*%llu\n", number, factors[0], factors[1]);*/
+		/*factorize(number);*/
+		factors = factorize(number);
+		printf("%llu=%llu*%llu\n", number, factors[0], factors[1]);
 	}
 
 	fclose(fp);
